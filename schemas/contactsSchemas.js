f517c40f3a3b6c24cc0eb601d9a7b315d4c8356d
PetroMazuryk <109526725+PetroMazuryk@users.noreply.github.com>
2025-10-10 01:25:07 +0300

    add favorite and priority fields to Joi schemas createContact and updateContact

--- schemas/contactsSchemas.js
+++ schemas/contactsSchemas.js
@@ -4,11 +4,14 @@ export const createContactSchema = Joi.object({
   name: Joi.string().required(),
   phone: Joi.string().required(),
   favorite: Joi.boolean(),
+  priority: Joi.boolean(),
 });
 
 export const updateContactSchema = Joi.object({
   name: Joi.string().required(),
   phone: Joi.string().required(),
+  favorite: Joi.boolean(),
+  priority: Joi.boolean(),
 });
 
 export const updateFavoriteSchema = Joi.object({
