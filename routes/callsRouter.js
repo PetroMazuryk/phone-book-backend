10868827f5c9b8ad6cae97611d5aa73b14b6fe9d
PetroMazuryk <109526725+PetroMazuryk@users.noreply.github.com>
2025-10-22 00:45:16 +0300

    add POST route for addCall with request body validation

--- routes/callsRouter.js
+++ routes/callsRouter.js
@@ -1,10 +1,16 @@
 import express from "express";
+import validateBody from "../helpers/validateBody.js";
 import ctrl from "../controllers/callsControllers.js";
+import { createCallSchema } from "../schemas/callsSchemas.js";
 
 const callsRouter = express.Router();
 
 callsRouter.delete("/:contactId/calls/:callId", ctrl.deleteCall);
 
-callsRouter.post("/:contactId/calls", ctrl.addCall);
+callsRouter.post(
+  "/:contactId/calls",
+  validateBody(createCallSchema),
+  ctrl.addCall
+);
 
 export default callsRouter;
