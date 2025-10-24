5c8ab5dc7f2ec718069a41028d7f316ddaee5038
PetroMazuryk <109526725+PetroMazuryk@users.noreply.github.com>
2025-10-24 17:24:20 +0300

    add route for editing a call by contactId and callId

--- routes/callsRouter.js
+++ routes/callsRouter.js
@@ -1,7 +1,7 @@
 import express from "express";
 import validateBody from "../helpers/validateBody.js";
 import ctrl from "../controllers/callsControllers.js";
-import { createCallSchema } from "../schemas/callsSchemas.js";
+import { createCallSchema, editCallSchema } from "../schemas/callsSchemas.js";
 
 const callsRouter = express.Router();
 
@@ -11,6 +11,12 @@ callsRouter.post(
   "/:contactId/calls",
   validateBody(createCallSchema),
   ctrl.addCall
+);
+
+callsRouter.put(
+  "/:contactId/calls/:callId",
+  validateBody(editCallSchema),
+  ctrl.editCall
 );
 
 export default callsRouter;
