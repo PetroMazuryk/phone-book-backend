117aad841bae1bfbd24dace7dad5c322d3a1f5c4
PetroMazuryk <109526725+PetroMazuryk@users.noreply.github.com>
2025-10-27 14:20:18 +0200

    add loginUser controller

--- controllers/usersControllers.js
+++ controllers/usersControllers.js
@@ -1,4 +1,4 @@
-import { createUser } from "../services/usersServices.js";
+import { createUser, findUserByEmail } from "../services/usersServices.js";
 import { ctrlWrapper } from "../helpers/ctrlWrapper.js";
 
 export const registerUser = async (req, res) => {
@@ -21,6 +21,31 @@ export const registerUser = async (req, res) => {
   });
 };
 
+export const loginUser = async (req, res) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    res.status(400).json({ message: "Email and password are required." });
+    return;
+  }
+
+  const user = findUserByEmail(email);
+
+  if (!user || user.password !== password) {
+    res.status(401).json({ message: "Invalid email or password." });
+    return;
+  }
+
+  res.status(200).json({
+    message: "Login successful",
+    user: {
+      id: user.id,
+      email: user.email,
+    },
+  });
+};
+
 export default {
   registerUser: ctrlWrapper(registerUser),
+  loginUser: ctrlWrapper(loginUser),
 };
