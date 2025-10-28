53ba77a8e3484ab7029de2b7ba93f65e619da4d8
PetroMazuryk <109526725+PetroMazuryk@users.noreply.github.com>
2025-10-28 17:50:45 +0200

    add userRegisterSchema

--- routes/usersRouter.js
+++ routes/usersRouter.js
@@ -1,10 +1,16 @@
 import express from "express";
 
 import ctrl from "../controllers/usersControllers.js";
+import validateBody from "../helpers/validateBody.js";
+import { userRegisterSchema } from "../schemas/usersSchemas.js";
 
 const usersRouter = express.Router();
 
-usersRouter.post("/register", ctrl.registerUser);
+usersRouter.post(
+  "/register",
+  validateBody(userRegisterSchema),
+  ctrl.registerUser
+);
 usersRouter.post("/login", ctrl.loginUser);
 
 export default usersRouter;
