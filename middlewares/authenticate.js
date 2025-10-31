3f645dbd3b09e0821e6e8dad0451fe980a02d0d2
PetroMazuryk <109526725+PetroMazuryk@users.noreply.github.com>
2025-10-31 14:25:10 +0200

    add authenticate function

---
+++ middlewares/authenticate.js
@@ -0,0 +1,25 @@
+import jwt from "jsonwebtoken";
+import { findUserById } from "../services/usersServices.js";
+
+const SECRET_KEY = process.env;
+
+export const authenticate = (req, res, next) => {
+  const { authorization = "" } = req.headers;
+  const [bearer, token] = authorization.split(" ");
+
+  if (bearer !== "Bearer" || !token) {
+    return res.status(401).json({ message: "Not authorized" });
+  }
+
+  try {
+    const { id } = jwt.verify(token, SECRET_KEY);
+    const user = findUserById(id);
+    if (!user) {
+      return res.status(401).json({ message: "Not authorized" });
+    }
+    req.user = user;
+    next();
+  } catch {
+    res.status(401).json({ message: "Not authorized" });
+  }
+};
